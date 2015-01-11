c654af6221524241f9cd88703a6b4aa7c829af0e
Skyang <hhyuestc@gmail.com>
2015-01-11 17:24:08 +0800

    注册

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
  */
 
 var express = require('express');
-var routes = require('./routes/index');
 var http = require('http');
 var path = require('path');
 var settings = require('./settings');
+var logger = require('morgan');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -16,33 +20,55 @@ var app = express();
 app.set('port', process.env.PORT || 2015);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon(__dirname + '/public/images/icon.png'));
-app.use(express.logger('dev'));
-app.use(express.json());            //json、urlencoded这两行
-app.use(express.urlencoded());      // 相当于以前的app.use(express.bodyParser())
-app.use(express.methodOverride());
-app.use(app.router);                //调用路径解析的规则
+app.use(favicon(__dirname + '/public/images/icon.png'));
+app.use(bodyParser.json());            //json、urlencoded这两行
+app.use(bodyParser.urlencoded({extended: false}));    // 相当于以前的app.use(express.bodyParser())
 app.use(express.static(path.join(__dirname, 'public')));    //设置根目录下的public为存放images,js,css等静态文件的文件夹
+app.use(cookieParser());
+app.use('/', routes);
 app.use(session({
     secret: settings.cookieSecret,
     key: settings.db,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24
+        maxAge: 1000 * 60 * 60 * 24 * 30
     },
     store: new MongoStore({
-        db: settings.db,
-        host: settings.host,
-        port: settings.port
+        db: settings.db
     })
 }));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-routes(app);
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('服务器已打开！本地端口为：' + app.get('port'));
 });
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,91 @@
 /*
  * GET home page.
  */
-module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.render('index');
+var express = require('express');
+var crypto = require('crypto');
+var router = express.Router();
+var User = require('../modles/users.js');
+var ejs = require('ejs');
+router.get("/", function (req, res) {
+    res.render('index');
+});
+router.get('/login', function (req, res) {
+    res.render('login');
+});
+router.post('/login', function (req, res) {
+    //生成密码的 md5 值
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
+    //检查用户是否存在
+    User.get(req.body.id, function (err, user) {
+        console.log(user);
+        if (!user) {
+            return res.redirect('/login');//用户不存在则跳转到登录页
+        }
+        //检查密码是否一致
+        if (user.password != password) {
+            return res.redirect('/login');//密码错误则跳转到登录页
+        }
+        //用户名密码都匹配后，将用户信息存入 session
+        req.session.user = user;
+        res.redirect('/');//登陆成功后跳转到主页
     });
-    app.get('/login', function (req, res) {
-        res.render('login');
+});
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+router.post('/register', function (req, res) {
+    var name = req.body.name,
+        id = req.body.id,
+        password = req.body.password,
+        passwordRepeat = req.body['passwordRepeat'],
+        email = req.body.email;
+    //检验用户两次输入的密码是否一致
+    if (passwordRepeat != password) {
+        //req.session.messages = ['error', '两次输入的密码不一致!'];
+        //req.flash('error', '两次输入的密码不一致!');
+        return res.redirect('/register');//返回注册页
+    }
+    //生成密码的 md5 值
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
+    var newUser = new User({
+        name: name,
+        id: id,
+        password: password,
+        email: email
     });
-    app.get('/register', function (req, res) {
-        res.render('register');
+    //检查用户名是否已经存在
+    User.get(newUser.name, function (err, user) {
+        if (err) {
+            //req.session.messages = ['error', err];
+            //req.flash('error', err);
+            return res.redirect('/');
+        }
+        if (user) {
+            //req.session.messages = ['error', '用户已存在!'];
+            //req.flash('error', '用户已存在!');
+            return res.redirect('/register');//返回注册页
+        }
+        //如果不存在则新增用户
+        newUser.save(function (err, user) {
+            if (err) {
+                //req.session.messages = ['error', err];
+                //req.flash('error', err);
+                return res.redirect('/register');//注册失败返回主册页
+            }
+            console.log(user);
+            req.session.user = user;//用户信息存入 session
+            //req.session.messages = ['success', '注册成功!'];
+            //req.flash('success', '注册成功!');
+            res.redirect('/');//注册成功后返回主页
+        });
     });
-    app.get('/logout', function (req, res) {
-        res.render('logout');
-    });
-    app.get('/blog', function (req, res) {
-        res.render('blog');
-    });
-};
+});
+router.get('/logout', function (req, res) {
+    res.render('logout');
+});
+router.get('/blog', function (req, res) {
+    res.render('blog');
+});
+module.exports = router;
